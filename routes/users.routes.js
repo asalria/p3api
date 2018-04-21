5cf85e844131bd269ea649e4cf4f838f2ee79535
Amelia Salgado <amelie@MacBook-Pro-de-Amelia.local>
2018-04-21 11:40:51 +0200

    fix: routes

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 const usersController = require('../controllers/users.controller');
+const secureMiddleware = require('../middleware/secure.middleware');
 
 router.post('/', usersController.create);
-router.get('/', usersController.list);
+router.get('/', secureMiddleware.isAuthenticated, usersController.list);
 router.get('/:id', usersController.listOne);
 router.put('/:id', usersController.edit);
 
